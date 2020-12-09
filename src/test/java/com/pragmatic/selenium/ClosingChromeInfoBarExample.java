cf117e4026c776dc1cfa0c9a747138e338255407
pragmatictesters <janesh@pragmatictesters.com>
2020-12-09 23:31:35 +0530

    Added example for closing Chrome Infobar

---
+++ src/test/java/com/pragmatic/selenium/ClosingChromeInfoBarExample.java
@@ -0,0 +1,41 @@
+package com.pragmatic.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Collections;
+
+/**
+ * Created by Pragmatic Test Labs (Private) Limited
+ *
+ * @Author Janesh Kodikara
+ */
+public class ClosingChromeInfoBarExample {
+
+
+    public static void main(String[] args) {
+
+        //Setup the browser driver
+        WebDriverManager.chromedriver().setup();
+
+        //Create an instance of Chrome browser browser
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("useAutomationExtension", false);
+        options.setExperimentalOption("excludeSwitches",
+                Collections.singletonList("enable-automation"));
+
+        WebDriver driver = new ChromeDriver(options);
+
+
+        //Navigate to the login page
+        driver.get("http://hrm.pragmatictestlabs.com/");
+
+
+
+
+    }
+
+
+}
