931afa095e82e1d384e19e64ed056fce8d61418c
pragmatictesters <janesh@pragmatictesters.com>
2020-12-13 18:38:26 +0530

    Added template for Selenium examples

---
+++ src/test/java/com/pragmatic/examples/selenium/SeleniumTemplate.java
@@ -0,0 +1,45 @@
+package com.pragmatic.examples.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Pragmatic Test Labs (Private) Limited
+ *
+ * @Author Janesh Kodikara
+ */
+public class SeleniumTemplate {
+
+
+    private static final String BASE_URL = "https://www.daraz.lk/";
+    private ChromeDriver driver;
+
+
+    @BeforeClass
+    public void beforeClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = new ChromeDriver();
+        driver.get(BASE_URL);
+    }
+
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.close();
+    }
+
+
+    @Test
+    public void testProductDiscount() {
+        System.out.println("SeleniumTemplate.testProductDiscount");
+    }
+
+}
