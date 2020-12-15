32e66a595999a6d442b9cdebda519855adbfb5b7
pragmatictesters <janesh@pragmatictesters.com>
2020-12-15 15:13:51 +0530

    added dragAndDropBy method

--- src/test/java/com/pragmatic/examples/selenium/DragAndDropExample.java
+++ src/test/java/com/pragmatic/examples/selenium/DragAndDropExample.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -36,12 +37,12 @@ public class DragAndDropExample {
 
     @AfterMethod
     public void afterMethod() {
-        driver.close();
+        //driver.close();
     }
 
 
     @Test
-    public void testProductDiscount() {
+    public void testDragAndDrop() {
         System.out.println("SeleniumTemplate.testProductDiscount");
 
         //Get the source element
@@ -56,7 +57,38 @@ public class DragAndDropExample {
         actions.dragAndDrop(srcElement, targetElemet)
                 .perform();
 
+        //Verifying the inner text within the target element after dropping the element
+        Assert.assertEquals(targetElemet.findElement(By.tagName("p")).getText(), "Dropped!");
+
 
     }
+
+
+    @Test
+    public void testDragAndDropToCorner() {
+        System.out.println("SeleniumTemplate.testProductDiscount");
+
+        //Get the source element
+        WebElement srcElement = driver.findElement(By.id("draggableview"));
+
+        //Get the target element
+        WebElement targetElement = driver.findElement(By.id("droppableview"));
+
+        int offsetX = targetElement.getLocation().x - srcElement.getLocation().x;
+        int offsetY = targetElement.getLocation().y - srcElement.getLocation().y;
+
+
+        //Drag and Drop to the left corner of the target element
+        Actions actions = new Actions(driver);
+
+        actions.dragAndDropBy(srcElement, offsetX, offsetY)
+                .perform();
+
+        //Verifying the inner text within the target element after dropping the element
+        Assert.assertEquals(targetElement.findElement(By.tagName("p")).getText(), "Dropped!");
+
+
+    }
+
 
 }
