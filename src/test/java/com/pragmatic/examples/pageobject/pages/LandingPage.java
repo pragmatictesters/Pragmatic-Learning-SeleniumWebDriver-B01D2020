ecab2ddc994ccf56b4ec0bb94aa4fce3da57f1c6
pragmatictesters <janesh@pragmatictesters.com>
2020-12-16 13:38:28 +0530

    Moved PageObject examples to a separate package

---
+++ src/test/java/com/pragmatic/examples/pageobject/pages/LandingPage.java
@@ -0,0 +1,23 @@
+package com.pragmatic.examples.pageobject.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Pragmatic Test Labs (Private) Limited
+ *
+ * @Author Janesh Kodikara
+ */
+public class LandingPage {
+
+    private final WebDriver driver;
+
+    public LandingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getWelcomeMessage() {
+        String welcomeMessage = driver.findElement(By.id("welcome")).getText();
+        return welcomeMessage;
+    }
+}
---
+++ src/test/java/com/pragmatic/examples/pageobject/pages/LoginPage.java
@@ -0,0 +1,39 @@
+package com.pragmatic.examples.pageobject.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Pragmatic Test Labs (Private) Limited
+ *
+ * @Author Janesh Kodikara
+ */
+public class LoginPage {
+
+    private final WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public LoginPage typeUsername(String username) {
+        driver.findElement(By.id("txtUsername")).sendKeys(username);
+        return this;
+
+    }
+
+    public LoginPage typePassword(String password) {
+        driver.findElement(By.id("txtPassword")).sendKeys(password);
+        return this;
+    }
+
+    public void clickLogin() {
+        driver.findElement(By.id("btnLogin")).click();
+    }
+
+    public String getError() {
+        String errorMessage = driver.findElement(By.id("spanMessage")).getText();
+        return errorMessage;
+
+    }
+}
